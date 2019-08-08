8fe4ed9e133ce8c7f166467f5defaf385c922e66
Aiorosu-cn <blueofsu@126.com>
2019-08-08 08:28:40 +0800

    1

--- C++/c++plus_PDF/test.cpp
+++ C++/c++plus_PDF/test.cpp
@@ -1,59 +1,57 @@
-class Queue
+#include <iostream>
+using namespace std;
+
+class one
 {
-private:
-	struct Node{ Item item; struct Node * next;}
-	enum{ Q_SIZE = 10};
-	Node * front ;
-	Node * rear;
-	int items;
-	const int qsize;
-public：
+	private:
+		double i;
+	public:
+		one(double n):i(n){};
+		one(){};
+		virtual void show(){cout << i;};
+};
+
+class onepluse:public one
+{
+	private:
+		double i;
+	public:
+		virtual void show(){cout << i+0.01;};
+		onepluse(double n):i(n){};
+};
+
+int main()
+{
+	one * test[2];
+	double num;
+	int ty;
+	for ( int i = 0 ; i < 2; i++)
+	{
+		cout << " input 1 use class one ,and input 2 use class oneplus! \n" << endl; 
+		cin >>  ty;
+		if ( ty == 1)
+		{
+			cout << "please input number  : ";
+			cin >>  num ;
+			test[i] = &one(num);
+		}
+		else if (ty == 2)
+		{
+			
+			cout << "please input number  : ";
+			cin >>  num ;
+			test[i] = &onepluse(num);
+		}
+	}
+	cout << "\n" << endl;
+	for (int i = 0; i < 2; i++)
+	{
+		test[i]->show();
+	}
 }
 
-Queue::Queue(int qs):qsize(qs)
-{
-	front = rear = NULL;
-	items = 0;
-	qsize = qs;
-}
 
 
-class Classy
-{
-	int mem1 = 10;		//in-class initialization
-	const int mem2 = 20;	//in-class initialization
-}
 
-Classy::Classy(): mem1(10), mem2(20){}
 
-bool Queue::enqueue(const Item & item)
-{
-	if (iffull())
-		return false;
-	Node * add = new Node;	//creat node 
-//on failure, new throws std::bad_alloc exception
-	add->item = item;
-	add->next = NULL;
-	items++;
-	if (front ==NULL)
-		front = add;
-	else
-		rear->next = add;
-	rear = add;
-	return true;
-}
-
-bool Queue::dequeue(Item & item)
-{
-	if(front == NULL)
-		return false;
-	item = front->item;
-	items--;
-	Node * temp = front;
-	front = front ->next;
-	delete temp;
-	f(items == 0)
-		rear = NULL;
-	return true;
-}
 
